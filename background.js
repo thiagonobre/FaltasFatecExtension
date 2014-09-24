23e62dd5d2faaa4750c1f1c83d44728c9a53fa54
Thiago Nobre <thiago.nobre2@gmail.com>
2014-09-24 15:06:53 -0300

    beta version

--- background.js
+++ background.js
@@ -1,5 +1,46 @@
 (function ($) {
-    $(function(){
-        console.log('Faltas Fatec');
+    $ (function () {
+        var table = $ ('#Grid1ContainerTbl');
+
+        var present = function (e) {
+            e.preventDefault ();
+            e.data.find ('input[type=checkbox]').attr ('checked', 'checked');
+        },
+        absent = function (e) {
+            e.preventDefault ();
+            e.data.find ('input[type=checkbox]').removeAttr ('checked');
+        }
+
+        table.find ('> tbody > tr').each (function (i) {
+            if (i == 0) {
+                return;
+            }
+
+            var tr = $ (this);
+
+            if (tr.find ('input:checkbox[disabled]').length) {
+                return;
+            }
+
+            var newTd = $ ('<td />');
+
+            var aPr = $ ('<a/>')
+                    .attr ('href', 'javascript:;')
+                    .unbind ('click')
+                    .click (tr, present)
+                    .css ('color', 'blue')
+                    .text ('Presente');
+            var aAb = $ ('<a/>')
+                    .attr ('href', 'javascript:;')
+                    .unbind ('click')
+                    .click (tr, absent)
+                    .css ('marginLeft', '10px')
+                    .css ('color', 'red')
+                    .text ('Ausente');
+
+            newTd.append ([aPr, aAb]);
+
+            tr.append (newTd);
+        })
     });
-})(jQuery);
+}) (jQuery);
